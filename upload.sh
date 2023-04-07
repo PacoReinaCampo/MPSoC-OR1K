dcbe928a56454beff287b65be4d24781bf63f99f
PacoReinaCampo <frareicam@gmail.com>
2023-04-08 01:08:57 +0200

    Working in MPSoC-OR1K 🔨

--- upload.sh
+++ upload.sh
@@ -42,6 +42,7 @@
 ##                                                                               ##
 ###################################################################################
 
+git submodule update --init --recursive --remote
 git add *
 git commit -m "Working in MPSoC-OR1K 🔨"
 git push origin master
